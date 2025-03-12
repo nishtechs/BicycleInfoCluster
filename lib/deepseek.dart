a620149abd674c0b24d1966e14e0679fe759fe34
Nishant <techinish@gmail.com>
2025-03-12 16:41:57 +0530

    ocr implemented not tested

---
+++ lib/deepseek.dart
@@ -0,0 +1,109 @@
+import 'package:deepseek_api/deepseek_api.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Deepseek extends StatelessWidget {
+  const Deepseek({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const BicycleComputerScreen();
+  }
+}
+
+class BicycleComputerScreen extends StatefulWidget {
+  const BicycleComputerScreen({super.key});
+
+  @override
+  _BicycleComputerScreenState createState() => _BicycleComputerScreenState();
+}
+
+class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
+  final TextEditingController _textController = TextEditingController();
+  String _displayText = '';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  String resp = "";
+
+  final deepseek = DeepSeekAPI(
+    apiKey: 'sk-a33aab42c73244e6afc5ca2e3cb1dbb7',
+    // Optional: baseUrl: 'https://api.deepseek.com/v1' (default)
+  );
+
+  callDeepseek(String query) async {
+    final response = await deepseek.createChatCompletion(
+      ChatCompletionRequest(
+        model: 'deepseek-chat',
+        messages: [ChatMessage(role: 'user', content: query)],
+        temperature: 0.7,
+        maxTokens: 100,
+      ),
+    );
+
+    setState(() {
+      resp = response.choices.first.message.content;
+    });
+    // print(response.choices.first.message.content);
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    void submitText() {
+      setState(() {
+        _displayText = _textController.text;
+      });
+      callDeepseek(_displayText);
+    }
+
+    return Scaffold(
+      backgroundColor: Colors.blueAccent[100],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Deepseek Page',
+          style: GoogleFonts.poppins(fontSize: 25.sp, fontWeight: FontWeight.w600, fontFeatures: [const FontFeature.alternativeFractions()], color: Colors.black),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                    labelText: resp,
+                    border: const OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 20.h),
+                ElevatedButton(
+                  onPressed: submitText,
+                  child: const Text('Submit'),
+                ),
+                SizedBox(height: 20.h),
+                Text(
+                  _displayText,
+                  style: TextStyle(fontSize: 18.sp),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_compass/flutter_compass.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:geolocator/geolocator.dart';
-// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:math';
-
 import 'package:google_fonts/google_fonts.dart';
+
+import 'deepseek.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,11 +18,9 @@ class MyApp extends StatelessWidget {
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
-      // Use builder only if you need to use library outside ScreenUtilInit context
       builder: (_, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          // You can use the library anywhere in the app even in theme
           theme: ThemeData(
             primarySwatch: Colors.deepPurple,
             textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
@@ -32,24 +30,6 @@ class MyApp extends StatelessWidget {
       },
       child: const BicycleComputerScreen(),
     );
-
-/*    return ScreenUtilInit(
-        designSize: const Size(360, 690),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        // Use builder only if you need to use library outside ScreenUtilInit context
-        builder: (_, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Bicycle Computer',
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-            ),
-            home: BicycleComputerScreen(),
-          );
-        },
-      child
-        );*/
   }
 }
 
@@ -61,19 +41,18 @@ class BicycleComputerScreen extends StatefulWidget {
 }
 
 class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
-  double _speed = 0.0;
-  double _distance = 0.0;
+  double _speed = 0.0; // Speed in km/h
+  double _distance = 0.0; // Distance in km
   double _heading = 0.0;
   String _direction = '';
   DateTime _currentTime = DateTime.now();
-  // FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  Position? _previousPosition; // Store the previous position for distance calculation
 
   @override
   void initState() {
     super.initState();
     _initCompass();
     _initLocation();
-    _initNotifications();
     _updateTime();
   }
 
@@ -93,16 +72,24 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
   void _initLocation() {
     Geolocator.getPositionStream().listen((Position position) {
       setState(() {
-        _speed = position.speed;
-        _distance += position.speed * (1 / 3600); // Update distance in km
+        // Convert speed from m/s to km/h
+        _speed = position.speed * 3.6;
+
+        // Calculate distance if there's a previous position
+        if (_previousPosition != null) {
+          double distanceInMeters = Geolocator.distanceBetween(
+            _previousPosition!.latitude,
+            _previousPosition!.longitude,
+            position.latitude,
+            position.longitude,
+          );
+          _distance += distanceInMeters / 1000; // Convert to km
+        }
+
+        // Update the previous position
+        _previousPosition = position;
       });
     });
-  }
-
-  void _initNotifications() {
-/*    var initializationSettingsAndroid = const AndroidInitializationSettings('@mipmap/ic_launcher');
-    var initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
-    flutterLocalNotificationsPlugin.initialize(initializationSettings);*/
   }
 
   void _updateTime() {
@@ -110,22 +97,6 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
       _currentTime = DateTime.now();
     });
     Future.delayed(const Duration(seconds: 1), _updateTime);
-  }
-
-  void _setAlarm() async {
-  /*  var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
-      'alarm_channel',
-      'Alarm Channel',
-      importance: Importance.max,
-      priority: Priority.high,
-    );
-    var platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
-      0,
-      'Alarm',
-      'Time to take a break!',
-      platformChannelSpecifics,
-    );*/
   }
 
   String _getDirection(double heading) {
@@ -160,7 +131,7 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
         centerTitle: true,
         title: Text(
           'Bikxter',
-          style: GoogleFonts.poppins(fontSize: 25.sp, fontWeight: FontWeight.w600, fontFeatures: [FontFeature.alternativeFractions()], color: Colors.black),
+          style: GoogleFonts.poppins(fontSize: 25.sp, fontWeight: FontWeight.w600, fontFeatures: [const FontFeature.alternativeFractions()], color: Colors.black),
         ),
       ),
       body: Center(
@@ -170,10 +141,18 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('${_currentTime.hour}:${_currentTime.minute}', style: GoogleFonts.poppins(fontSize: 30.sp, fontWeight: FontWeight.w600, color: Colors.lightGreenAccent)),
-                SizedBox(
-                  height: 5.h,
+                GestureDetector(
+                  onTap: () {
+                    navigateToDeepseek();
+                  },
+                  child: Card(
+                    child: Text(
+                      '${_currentTime.hour}:${_currentTime.minute}',
+                      style: GoogleFonts.poppins(fontSize: 30.sp, fontWeight: FontWeight.w600, color: Colors.lightGreenAccent),
+                    ),
+                  ),
                 ),
+                SizedBox(height: 5.h),
                 // Compass UI with Rotating Circle
                 SizedBox(
                   width: 250.w,
@@ -181,8 +160,6 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
                   child: Stack(
                     alignment: Alignment.center,
                     children: [
-                      // Rotating Compass Background
-
                       Transform.rotate(
                         angle: _heading * (pi / 180), // Convert degrees to radians
                         child: Image.asset(
@@ -192,8 +169,6 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
                           fit: BoxFit.contain,
                         ),
                       ),
-
-
                       Transform.rotate(
                         angle: _heading * (pi / -180), // Convert degrees to radians
                         child: Image.asset(
@@ -203,15 +178,11 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
                           fit: BoxFit.contain,
                         ),
                       ),
-
-                      // Static Arrow (Fixed)
                       Icon(
                         Icons.navigation,
                         size: 80.h,
                         color: Colors.red,
                       ),
-
-                      // Center Dot
                       Container(
                         width: 5.w,
                         height: 5.w,
@@ -225,21 +196,31 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
                 ),
                 SizedBox(height: 20.h),
                 // Speedometer
-                Text('Speed: ${_speed.toStringAsFixed(2)} km/h', style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black)),
+                Text(
+                  'Speed: ${_speed.toStringAsFixed(2)} km/h',
+                  style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black),
+                ),
                 SizedBox(height: 5.h),
                 // ODO Meter
-                Text('Distance: ${_distance.toStringAsFixed(2)} km', style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black)),
+                Text(
+                  'Distance: ${_distance.toStringAsFixed(2)} km',
+                  style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black),
+                ),
                 SizedBox(height: 5.h),
                 // Compass Heading and Direction
-                Text('Compass: ${_heading.toStringAsFixed(2)}°', style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black)),
+                Text(
+                  'Compass: ${_heading.toStringAsFixed(2)}°',
+                  style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black),
+                ),
                 SizedBox(height: 5.h),
-                Text('Direction: $_direction', style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black)),
-                SizedBox(height: 5.h),
-                // Current Time
+                Text(
+                  'Direction: $_direction',
+                  style: GoogleFonts.poppins(fontSize: 24.sp, color: Colors.black),
+                ),
                 SizedBox(height: 5.h),
                 // Alarm Button
                 ElevatedButton(
-                  onPressed: _setAlarm,
+                  onPressed: () {}, // Add your alarm logic here
                   child: const Text('Set Alarm'),
                 ),
               ],
@@ -248,5 +229,9 @@ class _BicycleComputerScreenState extends State<BicycleComputerScreen> {
         ),
       ),
     );
+  }
+
+  navigateToDeepseek() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => Deepseek()));
   }
 }
