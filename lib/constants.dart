efe7af49e669ed6d00de4c2cc5ee144ccd926434
Nishant <techinish@gmail.com>
2025-03-17 18:43:27 +0530

    ocr implementation

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,5 @@
+/*
 String openAIKeys = "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";
-String deepseekAIKeys = "sk-a33aab42c73244e6afc5ca2e3cb1dbb7";
+String deepseekAIKeys = "sk-a33aab42c73244e6afc5ca2e3cb1dbb7";*/
+String openAIKeys = "";
+String deepseekAIKeys = "";
